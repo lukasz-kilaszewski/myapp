6fb6b2679a0c41960e8e080857d7fba469246a4d
Lukasz Kilaszewski <lkilaszewski@gmail.com>
2018-05-11 20:23:36 +0200

    mockup and unnecessary things

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,5 +1,4 @@
 class QuotationsController < ApplicationController
-  before_action :set_quotation, only: [:show, :edit, :update, :destroy]
 
   # GET /quotations
   # GET /quotations.json
@@ -7,20 +6,10 @@ class QuotationsController < ApplicationController
     @quotations = Quotation.all
   end
 
-  # GET /quotations/1
-  # GET /quotations/1.json
-  def show
-  end
-
   # GET /quotations/new
   def new
     @quotation = Quotation.new
   end
-
-  # GET /quotations/1/edit
-  def edit
-  end
-
   # POST /quotations
   # POST /quotations.json
   def create
@@ -28,8 +17,8 @@ class QuotationsController < ApplicationController
 
     respond_to do |format|
       if @quotation.save
-        format.html { redirect_to @quotation, notice: 'Quotation was successfully created.' }
-        format.json { render :show, status: :created, location: @quotation }
+        format.html { redirect_to root_path, notice: 'Quotation was successfully created.' }
+        format.json { render :nil, status: :created, location: root_path }
       else
         format.html { render :new }
         format.json { render json: @quotation.errors, status: :unprocessable_entity }
@@ -37,35 +26,9 @@ class QuotationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /quotations/1
-  # PATCH/PUT /quotations/1.json
-  def update
-    respond_to do |format|
-      if @quotation.update(quotation_params)
-        format.html { redirect_to @quotation, notice: 'Quotation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @quotation }
-      else
-        format.html { render :edit }
-        format.json { render json: @quotation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /quotations/1
-  # DELETE /quotations/1.json
-  def destroy
-    @quotation.destroy
-    respond_to do |format|
-      format.html { redirect_to quotations_url, notice: 'Quotation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_quotation
-      @quotation = Quotation.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quotation_params
